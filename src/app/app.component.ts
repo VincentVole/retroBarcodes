720f34c75bcf63c5bb77d8339edb5fb44d248f4c
Vincent Vole <volevincent@gmail.com>
2017-08-08 17:57:23 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  colors = [
+  	'DeepPink',
+  	'Aqua',
+  	'Brown',
+  	'Chartreuse',
+  	'Gold',
+  	'MediumSlateBlue'
+  ]
+  boxes = [];
+  numBoxes = 6;
+  constructor(){
+  	for(var i = 0; i < this.numBoxes; i++){
+  		this.boxes.push(this.colors[Math.floor(Math.random() * this.colors.length)]);
+  	}
+  }
 }
